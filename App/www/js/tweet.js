f4167823af98cfe03cd8ae351bef21f9e84b3d24
dwladdimiroc <daniel.wladdimiro@gmail.com>
2014-10-09 13:03:11 -0300

    Fix checklist

--- App/www/js/tweet.js
+++ App/www/js/tweet.js
@@ -28,7 +28,7 @@ function totalTweet(){
 
     $.ajax({ url: urlTweet, type: 'GET', dataType: "json",
     success: function(resultData) {
-         console.log('Result Data: '+resultData.numTweet);
+         //console.log('Result Data: '+resultData.numTweet);
          window.localStorage.setItem("numTweet", resultData.numTweet);
     },
     error: function(xhr, status, error){
@@ -46,7 +46,7 @@ function initTweet() {
     var randomID = randomTweet();
     if(randomID != -1){
         window.localStorage.setItem("idTweet", randomID);
-        console.log('RandomID: ' + randomID);
+        //console.log('RandomID: ' + randomID);
         checkTweet(randomID);
     } else {
         document.getElementById("tweetText").innerHTML = 'No hay más tweet que analizar';
@@ -71,10 +71,8 @@ function randomTweet() {
     return random;
 }
 
-//Check list de la clasificación de cada tweet
-$(function () {
+function clearCheckList(){
     $('.list-group.checked-list-box .list-group-item').each(function () {
-        
         // Settings
         var $widget = $(this),
             $checkbox = $('<input type="checkbox" class="hidden" />'),
@@ -87,21 +85,7 @@ $(function () {
                 off: {
                     icon: 'glyphicon glyphicon-unchecked'
                 }
-            };
-            
-        $widget.css('cursor', 'pointer')
-        $widget.append($checkbox);
-
-        // Event Handlers
-        $widget.on('click', function () {
-            $checkbox.prop('checked', !$checkbox.is(':checked'));
-            $checkbox.triggerHandler('change');
-            updateDisplay();
-        });
-        $checkbox.on('change', function () {
-            updateDisplay();
-        });
-          
+            };          
 
         // Actions
         function updateDisplay() {
@@ -137,9 +121,80 @@ $(function () {
                 $widget.prepend('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
             }
         }
+
         init();
     });
-    
+}
+
+//Check list de la clasificación de cada tweet
+$(function () {
+    $('.list-group.checked-list-box .list-group-item').each(function () {
+        
+        // Settings
+        var $widget = $(this),
+            $checkbox = $('<input type="checkbox" class="hidden" />'),
+            color = ($widget.data('color') ? $widget.data('color') : "primary"),
+            style = ($widget.data('style') == "button" ? "btn-" : "list-group-item-"),
+            settings = {
+                on: {
+                    icon: 'glyphicon glyphicon-check'
+                },
+                off: {
+                    icon: 'glyphicon glyphicon-unchecked'
+                }
+            };
+            
+        $widget.css('cursor', 'pointer')
+        $widget.append($checkbox);
+
+        // Event Handlers
+        $widget.on('click', function () {
+            $checkbox.prop('checked', !$checkbox.is(':checked'));
+            $checkbox.triggerHandler('change');
+            updateDisplay();
+        });
+
+        $checkbox.on('change', function () {
+            updateDisplay();
+        });
+          
+        // Actions
+        function updateDisplay() {
+            var isChecked = $checkbox.is(':checked');
+
+            // Set the button's state
+            $widget.data('state', (isChecked) ? "on" : "off");
+
+            // Set the button's icon
+            $widget.find('.state-icon')
+                .removeClass()
+                .addClass('state-icon ' + settings[$widget.data('state')].icon);
+
+            // Update the button's color
+            if (isChecked) {
+                $widget.addClass(style + color + ' active');
+            } else {
+                $widget.removeClass(style + color + ' active');
+            }
+        }
+
+        // Initialization
+        function initChecklist() {
+            
+            if ($widget.data('checked') == true) {
+                $checkbox.prop('checked', !$checkbox.is(':checked'));
+            }
+            
+            updateDisplay();
+
+            // Inject the icon if applicable
+            if ($widget.find('.state-icon').length == 0) {
+                $widget.prepend('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
+            }
+        }
+
+        initChecklist();
+    }); 
 });
 
 //Envío de los datos para actualizar la página
@@ -184,6 +239,7 @@ $(document).ready(function(){
                 var arrayRandom = JSON.parse(window.localStorage.getItem("arrayRandom"));
                 arrayRandom.push(idTweet);
                 window.localStorage.setItem("arrayRandom", JSON.stringify(arrayRandom));
+                clearCheckList();
                 initTweet();
             },
             error: function(req,error) { 
